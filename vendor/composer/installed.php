3b2959aec9ff19e18fc2cc9ee369ddfda22d414c
Lucas Alexsander <lucas10barbosa_@hotmail.com>
2022-05-10 10:55:42 -0300

    teste

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'lybraty',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0237542aa7d215ee082b2adc6d876eb4dae048fb',
+        'name' => 'lucasalexsander/smileface',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lucasalexsander/smileface' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'lybraty',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0237542aa7d215ee082b2adc6d876eb4dae048fb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
